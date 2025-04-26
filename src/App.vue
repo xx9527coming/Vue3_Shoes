275d4a76659853514c69e092205b5a385a1f6811
xx9527coming <348960212@qq.com>
2025-04-26 22:13:21 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,12 @@
 <template>
     <div class="app">
- <!--   <h1>你好啊！</h1>
-        <Person/>
-        <h1>Cat作业</h1>
-        <Cat/>
-        <h1>Car案例</h1>
-        <Car/> -->
-        <!-- <h1>Name案例</h1>
-        <Name/> -->
         <h1>Shoes案例</h1>
         <Shoes/>
     </div>
 </template>
 
 <script setup lang="ts">  
-/* import Person from './components/Person.vue'
-import Cat from './components/Cat.vue'
-import Car from './components/Car.vue'; */
-/* import Name from './components/Name.vue'; */
 import Shoes from './components/Shoes.vue';
-
-
-/*    export default {
-    name:'App', //组件名
-    components:{Person,Cat,Car}//组件注册
-  } */
-
 
 </script>
 
